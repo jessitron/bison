b892eb847dd3e20b85a20b87701f4c8dfe6653fb
Jessica Kerr <jessitron@gmail.com>
2013-11-26 13:16:08 -0600

    Added basic ranker #2

--- src/main/scala/jessitron/tweetbot/Rankers.scala
+++ src/main/scala/jessitron/tweetbot/Rankers.scala
@@ -5,9 +5,21 @@ import scala.util.Random
 
 object Rankers {
 
-  val randomo: Process1[Message, Message] = process1.lift { m: Message => m match {
-    case i: IncomingTweet => i.addMyTwoCents(Opinion(Random.nextDouble, Some("I agree!")))
+  val randomo: Process1[Message, Message] = simpleOpinion{_ => Some(Opinion(Random.nextDouble, Some("I agree!")))}
+
+  private def simpleOpinion(f: IncomingTweet => Option[Opinion]) = process1.lift { m: Message => m match {
+    case i: IncomingTweet => f(i) match {
+      case Some(opinion) => i.addMyTwoCents(opinion)
+      case _ => i
+    }
     case m => m
   }}
+
+  val FirstPersonSentence = ".*I ([^!?]*)[\\.!?].*".r
+  def iDoThisToo: Process1[Message, Message] = simpleOpinion { _.tweet.text match {
+      case FirstPersonSentence(words) => Some(Opinion(1.0, Some(s"I $words, too!")))
+      case _ => None
+    }
+  }
 
 }
--- src/test/scala/jessitron/tweetbot/RankerSpec.scala
+++ src/test/scala/jessitron/tweetbot/RankerSpec.scala
@@ -17,4 +17,61 @@ object RankerSpec extends Properties("Rankers") {
       (output.head.opinions.size ?= (1 + incoming.opinions.size)) :| "One more opinon"
   }
 
+  import Gen._
+  val presentTenseSentence: Gen[String] = for {
+    subj <- oneOf("I", "you", "@jessitron", "We", "My mom")
+    verb <- oneOf("love", "eat", "run", "code")
+    adverb <- oneOf("just","well","swimmingly","crazily")
+    preposition <- oneOf("with","in","among")
+    noun <- oneOf("pizza","pillows","babies","scala")
+    adjective <- oneOf("blue","smart","crazy")
+    structure <- choose(1,4)
+  } yield structure match {
+    case 1 => s"$subj $verb $adverb $preposition $noun"
+    case 2 => s"$subj is $adjective"
+    case 3 => s"$subj $verb $noun"
+    case 4 => s"$preposition $noun $subj $verb"
+  }
+
+  val realisticText: Gen[String] = for {
+    s1 <- presentTenseSentence
+    s2 <- presentTenseSentence
+    punctuation <- oneOf(".","!","?!?")
+    hashtag <- oneOf("scala", "lol", "sothere")
+    structure <- choose(1,3)
+  } yield structure match {
+    case 1 => s"$s1$punctuation"
+    case 2 => s"$s1 and $s2$punctuation #$hashtag"
+    case 3 => s"$s1$punctuation #$hashtag :-)"
+  }
+
+  val realisticTweet: Gen[IncomingTweet] = realisticText map {t => IncomingTweet(TweetDetail(t))}
+
+  property("Sentences that start with I") = forAll(realisticTweet) {
+    tweet =>
+      val p = Process(tweet) |> Rankers.iDoThisToo
+      val output = p.toList.head.asInstanceOf[IncomingTweet]
+
+      (output.opinions.size >= 1) ==> {
+        val newOpinion = output.opinions.head // would be a better test to get the new one
+
+        (newOpinion.points > 0) :| "Positive score" &&
+        (newOpinion.hasSuggestion) :| "has a suggestion" &&
+        (newOpinion.suggestedText.get.contains("I ")) :| "it's about me" &&
+        (newOpinion.suggestedText.get.contains(" too!")) :| "I agree"
+          // better: test that the stuff in between is also in the tweet
+
+      }  // making no statements if this ranker never expresses an opinion
+  }
+
+  property("Easy example") = {
+    val text = "I love matches!"
+    val tweet = IncomingTweet(TweetDetail(text))
+    val p = Process(tweet) |> Rankers.iDoThisToo
+    val output = p.toList.head.asInstanceOf[IncomingTweet]
+
+    output.opinions.head.suggestedText.get ?= "I love matches, too!"
+  }
+
+
 }
