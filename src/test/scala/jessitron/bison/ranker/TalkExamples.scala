87d3746052da62b7a427c9e0b9df428aa903270f
Jessica Kerr <jessitron@gmail.com>
2014-04-03 11:53:35 -0500

    Making talk examples

--- src/test/scala/jessitron/bison/ranker/TalkExamples.scala
+++ src/test/scala/jessitron/bison/ranker/TalkExamples.scala
@@ -4,20 +4,21 @@ import org.scalacheck._
 import jessitron.bison._
 import scalaz.stream.Process
 
-object CapitalizationRankerSpec extends Properties("CapitalizationRanker") {
+object ExampleSpec1 extends Properties("CapitalizationRanker 1") {
+  import TalkExamples._
 
-  // weakness: expressiveness - what is it about this input that makes the Bison agree?
   property("Example: all capitals") = {
-    // weakness: painful construction
     val oneTweet = tweetThatSays("I Am So Happy")
 
-    // weakness: we're only testing one tweet, and a process is designed to handle many
-    val output: List[IncomingTweet] = processThroughRanker(Seq(oneTweet))
+    val output = processThroughRanker(Seq(oneTweet))
 
-    // weakness of ScalaCheck: painful to get assertions to print well
     output.head.opinions.head == Opinion(1.0, Some("I Agree Completely"))
-    // weakness: does it always have to be exactly that?
   }
+
+}
+
+object ExampleSpec2 extends Properties("CapitalizationRanker 2") {
+  import TalkExamples._
 
   property("Example: no capitals") = {
     // weakness: duplication. Lots of it.
@@ -34,14 +35,15 @@ object CapitalizationRankerSpec extends Properties("CapitalizationRanker") {
 
   import Common._
   import Prop._
-
   property("Ranks tweets with more capitalized words higher") = forAll { listOfTweets: List[IncomingTweet] =>
     // well, we could add a general property of generators here.
     // We could also generate exactly one tweet, and then change it to have more capitalized words.
     // Even, to continue capitalizing and un-capitalizing, to make a whole list. The top of which has a suggestion.
      false
   }
+}
 
+object TalkExamples {
   def processThroughRanker(input: Seq[IncomingTweet]): List[IncomingTweet] = {
     val result = Process(input:_*) |> CapitalizationRanker()
     result.toList.map{_.asInstanceOf[IncomingTweet]}
